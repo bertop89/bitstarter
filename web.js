32a5e6be334118cdf86d133abad72791050fce73
Alberto Polidura <bertop@gmail.com>
2013-07-08 18:09:04 +0200

    reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = new Buffer(256);
+  fs.readFile('./index.html', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    response.send(buf.toString('utf8', 0, 256));
+  });
 });
 
 var port = process.env.PORT || 5000;
