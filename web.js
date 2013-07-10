b9182eafb8cdd84d17eb5e34b4681793a19b3d2c
Alberto Polidura <bertop@gmail.com>
2013-07-10 17:39:12 +0200

    Fixed?

--- web.js
+++ web.js
@@ -5,12 +5,9 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  buf = new Buffer(256);
-  fs.readFile('./index.html', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-    buf.write(data);
-  });
+  var buf = new Buffer(256);
+  data = fs.readFileSync('./index.html')
+  buf.write(data);
   buf.toString('utf-8')
 });
 
